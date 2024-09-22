41426926fda94d9b274dbba92d5299b764f295ab
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2024-09-22 00:31:56 -0300

    d

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const pergunta1 = [
     {
+        enunciado: ["Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
+        ]
+    },
+    {
+        enunciado: ["Pergunta 1",
+            alternativas: "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: ["Pergunta 1",
+            alternativas: "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
         enunciado: ["Pergunta 1",
             alternativas: "Alternativa 1",
             "Alternativa 2"
