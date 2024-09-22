f0dbe0b7a29ca34483dd05afc24b00401e0ae4e3
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2024-09-22 00:14:50 -0300

    d

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-Resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
