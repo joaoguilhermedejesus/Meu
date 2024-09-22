f87c5254c80c61635b3793c33f40d9f86e05130c
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2024-09-22 00:32:52 -0300

    d

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ const pergunta1 = [
         ]
     },
     {
-        enunciado: ["Pergunta 1",
-            alternativas: "Alternativa 1",
-            "Alternativa 2"
+        enunciado: ["Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
         ]
     },
     {
