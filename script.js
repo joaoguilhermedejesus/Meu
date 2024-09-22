351f16b1f34276e4effdbc9a7c117cd99ab3dd92
joaoguilhermedejesus <105368350+joaoguilhermedejesus@users.noreply.github.com>
2024-09-22 00:24:56 -0300

    g

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const pergunta1 = [
-{
-    enunciado: ["Pergunta 1",
-    alternativas: "Alternativa 1", 
-    "Alternativa 2"
-    ]
-},
-{
-    enunciado: ["Pergunta 1",
-    alternativas: "Alternativa 1", 
-    "Alternativa 2"
-    ]
-}
+    {
+        enunciado: ["Pergunta 1",
+            alternativas: "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: ["Pergunta 1",
+            alternativas: "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
 
-]
+];
